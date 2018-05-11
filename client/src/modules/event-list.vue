697c995a8eb68a7a1e9bdecd0970caa35482daec
Abhay Joshi <abhayj@synerzipune.local>
2018-05-11 17:24:49 +0530

    added style to show cards in horizontal for bigger screens

--- client/src/modules/event-list.vue
+++ client/src/modules/event-list.vue
@@ -3,23 +3,28 @@
     <h2>
       Events List
     </h2>
-    <b-card
-      header-border-variant="primary"
-      header-bg-variant="secondary"
-      header-text-variant="white"
-      class="my-3 shadow bg-white rounded event-card"
-      :header="event ? event.name : 'event'"
-      v-for="event in events" :key="event.id" 
-    >
-      <div class="line-content" :title="event.id">
-        <strong>Id:</strong>
-        {{event.id}}
-      </div>
-      <div class="line-content" :title="event.author">
-        <strong>Author:</strong>
-        {{event.author}}
-      </div>
-    </b-card>
+    <b-row>
+      <b-col sm="10" md="5" lg="4"
+        v-for="event in events" :key="event.id"      
+      >
+      <b-card 
+        header-border-variant="primary"
+        header-bg-variant="secondary"
+        header-text-variant="white"
+        class="my-3 shadow bg-white rounded event-card"
+        :header="event ? event.name : 'event'"
+      >
+        <div class="line-content" :title="event.id">
+          <strong>Id:</strong>
+          {{event.id}}
+        </div>
+        <div class="line-content" :title="event.author">
+          <strong>Author:</strong>
+          {{event.author}}
+        </div>
+      </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -38,27 +43,31 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+
 .event-list-container {
   text-align: left;
 }
 
-.line-content {
-  white-space: nowrap;  
+/* replace absolute media queries with bootstrap mixins */
+@media (max-width: 767px) {
+  .line-content {
+    white-space: nowrap;
+  }
+
+  .event-card {
+    overflow-x: auto;
+  }
 }
 
-.event-card {
-  overflow-x: auto;
+@media (min-width: 768px) {
+  .line-content {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
 }
-/* .card-header {
-  padding: 5px;
-} */
-/* 
-header text alignment
-.card-header > div {
-  padding-left: 15px;
-} 
-*/
+
 </style>
 
 
